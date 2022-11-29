9c47e833c1b3981d5580492f6e2d9b3de4e154a7
sarahangle <sna44@cornell.edu>
2022-11-28 23:19:43 -0800

    Complete assignment 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    if (array.length == 0) {return [];}
 
+    for (let i = 0; i < args.length; i++) {
+        const item = args[i];
+        if (array.indexOf(item) >= 0) {array.splice(array.indexOf(item), 1) }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
