4ba0c22c52baab82b6aa5a52c6f2672a2a6b9db3
sarahangle <sna44@cornell.edu>
2022-11-28 23:28:00 -0800

    Complete assignment 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof num1 != "number" || typeof num2 != "number") {return "ERROR";}
+    if (num1 < 0 || num2 < 0) {return "ERROR";}
 
+    let smaller = num1;
+    let larger = num2;
+    if (num2 < num1) {
+        smaller = num2;
+        larger = num1;
+    }
+
+    let sum = 0;
+    for (let i = smaller; i <= larger; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
