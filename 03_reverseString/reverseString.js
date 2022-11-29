45cc78b7e55d5a7b7d0f8bb4e2de9ce18ce0e546
sarahangle <sna44@cornell.edu>
2022-11-28 23:07:29 -0800

    Complete assignment 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    result = "";
+    for (let i = input.length-1; i >= 0; i--) {
+        result += input[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
