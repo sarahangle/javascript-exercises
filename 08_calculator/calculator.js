c8ec9d627173d9295a8c52dfe8e26d172c4a43e5
sarahangle <sna44@cornell.edu>
2022-12-04 17:58:35 -0800

    Complete assignment 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let init = 0
+  return array.reduce((acc, curr) => acc+curr, init);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let init = 1
+  return array.reduce((acc, curr) => acc*curr, init);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  quotient = 1;
+  while (num > 0) {
+    quotient *= num;
+    num--;
+  }
+  return quotient;
 };
 
 // Do not edit below this line
