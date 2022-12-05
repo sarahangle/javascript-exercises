3a603070045a4509393bd335ff461926c70c4331
sarahangle <sna44@cornell.edu>
2022-12-04 18:26:23 -0800

    Complete assignment 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (typeof num == "string") {num = parseInt(num);}
+    else if (num < 0) {return "OOPS";}
 
+    let fibonacciVals = [1,1];
+    while (num > fibonacciVals.length) {
+        const last = fibonacciVals[fibonacciVals.length - 1];
+        const nextLast = fibonacciVals[fibonacciVals.length - 2];
+        fibonacciVals.push(last + nextLast);
+    }
+    return fibonacciVals[fibonacciVals.length-1]
 };
 
 // Do not edit below this line
