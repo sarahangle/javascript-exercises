45a4201630070f7b98c335ab5dc12e35ef01b6d8
sarahangle <sna44@cornell.edu>
2022-12-04 18:44:08 -0800

    Complete assignment 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort((a,b) => {
+        let aAge = 0;
+        let bAge = 0;
+        if (!a.yearOfDeath) {
+            aAge = new Date().getFullYear() - a.yearOfBirth;
+        } else {
+            aAge = a.yearOfDeath - a.yearOfBirth;
+        }
+        if (!b.yearOfDeath) {
+            bAge = new Date().getFullYear() - b.yearOfBirth;
+        } else {
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        }
+        if (aAge >= bAge) {return -1;}
+        else {return 1;}
+    });
+    return people[0];
 };
 
 // Do not edit below this line
