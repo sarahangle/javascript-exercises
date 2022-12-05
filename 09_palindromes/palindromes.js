1ec95023905cf9b974c291fb050ed2d4d36b7d14
sarahangle <sna44@cornell.edu>
2022-12-04 18:20:15 -0800

    Complete assignment 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    let wordStripped = word.replace(/[^\w]/g, "").toLowerCase();
+    midpoint = Math.ceil(wordStripped.length/2) - 1;
+    for (let i = 0; i < midpoint; i++) {
+        if (wordStripped[i] != wordStripped[wordStripped.length-1-i]) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
